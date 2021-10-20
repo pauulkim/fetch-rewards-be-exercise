93a2703e9462299e455dc7e208932ff4722cf297
Paul Kim <paulwkim30@gmail.com>
2021-10-19 18:28:39 -0700

    create base express application

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Fetch Rewards Coding Exercise
 <!-- - `npm init -y` - creates package.json -->
-- `npm install` install dependencies
+- `npm install` install dependencies (from package.json)
 <!-- - `npm install express request-promise nodemon` -->
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-import express from "express";
+const express = require("express");
+const request = require("request-promise");
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
