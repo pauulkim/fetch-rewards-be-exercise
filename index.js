9e30ae8fdc5d951c845eeebce973967479036b39
Paul Kim <paulwkim30@gmail.com>
2021-10-20 15:47:46 -0700

    add additional routes

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
 
+import transactionRoutes from "./routes/transaction.js";
+
+// initialize the app and set a port
 const app = express();
 const PORT = 5000;
 
+// middleware 
 app.use(express.json()); 
 
+// homepage route
 app.get("/", (req, res) => {
   console.log("!TEST!")
-  res.send("Hello World!");
+  // res.send("Hello World!");
 });
 
+// api routes
+app.use("/transaction", transactionRoutes);
+
+// start the app
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
