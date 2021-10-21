1558c6d8b9c54c85d4d0cf357b4f151823b01b16
Paul Kim <paulwkim30@gmail.com>
2021-10-20 18:49:47 -0700

    modify balance route

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
   - uses nodemon which allows you to listen to changes
   - allows you to access `http://localhost:5000/`
 - `npm run start` to run the actual server
-- `curl http://localhost:5000/transactions/ ; echo` to get list of all transactions
 - `curl -X POST -H "Content-Type: application/json" -d '{ "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" }' http://localhost:5000/transactions/ ; echo`
   - to add a transaction to the transactions table
+- `curl http://localhost:5000/transactions/ ; echo` to get balances
--- routes/transactions.js
+++ routes/transactions.js
@@ -5,17 +5,25 @@ const router = express.Router();
 // "table" for all transactions
 const transactions = [];
 
-// get all the transactions
-router.get("/", (req, res) => {
-  res.json(transactions)
-});
-
 // add a transaction
 router.post("/", (req, res) => {
   transactions.push(req.body)
   res.json(req.body);
 });
 
+// get balance
+router.get("/", (req, res) => {
+  const formatted = {};
 
+  for (let transaction of transactions) {
+    if (formatted[transaction["payer"]]) formatted[transaction["payer"]] += transaction["points"]
+    else formatted[transaction["payer"]] = transaction["points"]
+  }
+
+  res.json(formatted);
+});
+
+
+// 
 
 export default router;
