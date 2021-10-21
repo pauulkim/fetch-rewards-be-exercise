d1956a1d3cce858d87cedd7e7ab8f58fea0e85e3
Paul Kim <paulwkim30@gmail.com>
2021-10-20 21:30:18 -0700

    update transactions table to reflect new amounts

--- routes/transactions.js
+++ routes/transactions.js
@@ -15,6 +15,7 @@ router.post("/", (req, res) => {
 router.get("/", (req, res) => {
   const formatted = {};
 
+  // go through each of the transactions and get the totals for each payer
   for (let transaction of transactions) {
     if (formatted[transaction.payer]) formatted[transaction.payer] += transaction.points
     else formatted[transaction.payer] = transaction.points
@@ -26,9 +27,29 @@ router.get("/", (req, res) => {
 
 // spending points
 router.post("/spend", (req, res) => {
-  transactions.sort( (a, b) =>  new Date(a.timestamp) - new Date(b.timestamp) )
+  let points = req.body.points;
+  const spend = {};
 
-  res.json(transactions);
+  // order transactions from oldest to newest
+  transactions.sort( (a, b) =>  new Date(a.timestamp) - new Date(b.timestamp) );
+
+  for (let t of transactions) {
+    if (points >= t.points) {
+      if (spend[t.payer]) spend[t.payer].points -= t.points
+      else spend[t.payer] = { "payer": t.payer, "points": - t.points };
+      points -= t.points;
+
+      t.points = 0;
+    } else {
+      spend[t.payer] = { "payer": t.payer, "points": - points };
+      points = 0;
+
+      t.points -= points;
+      break;
+    }
+  }
+
+  res.json(spend);
 });
 
 export default router;
